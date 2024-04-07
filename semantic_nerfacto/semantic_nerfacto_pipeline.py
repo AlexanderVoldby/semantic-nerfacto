d1653785e0e0267ff6ec762c3e03b1baf72fd229
AlexanderVoldby <alexander@murmann.dk>
2024-04-07 12:25:48 +0200

    Model works after some debugging. Results seem insignificant. Perhaps increase the weights?

--- semantic_nerfacto/semantic_nerfacto_pipeline.py
+++ semantic_nerfacto/semantic_nerfacto_pipeline.py
@@ -5,7 +5,7 @@ from regNeRF / nerfbusters is implemented
 import torch
 import typing
 from dataclasses import dataclass, field
-from typing import Literal, Optional, Type
+from typing import Literal, Optional, Type, Tuple
 from torchtyping import TensorType
 
 import torch.distributed as dist
@@ -14,7 +14,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 from semantic_nerfacto.semantic_nerfacto_datamanager import SemanticNerfactoDataManagerConfig
 from semantic_nerfacto.semantic_nerfacto import SemanticNerfactoModel, SemanticNerfactoModelConfig
-from utils.random_train_pose import random_train_pose
+from semantic_nerfacto.utils.random_train_pose import random_train_pose
 
 from nerfstudio.data.datamanagers.base_datamanager import (
     DataManager,
@@ -38,12 +38,31 @@ class SemanticNerfactoPipelineConfig(VanillaPipelineConfig):
     """specifies the datamanager config"""
     model: ModelConfig = SemanticNerfactoModelConfig()
     """specifies the model config"""
+    
+    # patch sampling
+    num_patches: int = 10
+    """Number of patches per batch for training"""
+    patch_resolution: int = 32
+    """Patch resolution, where DiffRF used 48x48 and RegNeRF used 8x8"""
+    focal_range: Tuple[float, float] = (3.0, 3.0)
+    """Range of focal length"""
+    central_rotation_range: Tuple[float, float] = (-180, 180)
+    """Range of central rotation"""
+    vertical_rotation_range: Tuple[float, float] = (-90, 20)
+    """Range of vertical rotation"""
+    jitter_std: float = 0.05
+    """Std of camera direction jitter, so we don't just point the cameras towards the center every time"""
+    center: Tuple[float, float, float] = (0, 0, 0)
+    """Center coordinate of the camera sphere"""
+    aabb_scalar: float = 1.5
+    
+    # Losses
     use_regnerf_depth_loss: bool = False
     """Whether to use reqgularization on depth patches"""
     use_regnerf_rgb_loss: bool = False
     """Whether to use regularization on RGB patches"""
     use_regnerf_semantics_loss: bool = False
-    """Whether to use regularization on RGB patches"""
+    """Whether to use regularization on semantics patches"""
     regnerf_depth_loss_mult = 1e-6
     regnerf_rgb_loss_mult = 1e-6
     regnerf_semantics_loss_mult = 1e-6
@@ -106,7 +125,8 @@ class SemanticNerfactoPipeline(VanillaPipeline):
         model_outputs, loss_dict, metrics_dict = super().get_train_loss_dict(step)
 
         # --------------------- 2D losses ---------------------
-        activate_patch_sampling = self.config.use_regnerf_loss
+        # Assume we always want patch sampling
+        activate_patch_sampling = True
 
         # TODO: debug why patch sampling decreases model performance
         if activate_patch_sampling:
@@ -129,7 +149,7 @@ class SemanticNerfactoPipeline(VanillaPipeline):
             )  # (patch_resolution, patch_resolution, num_patches)
             ray_bundle_patches = ray_bundle_patches.flatten()
             # TODO: deal with appearance embeddings
-            model_outputs_patches = self.model.get_outputs(ray_bundle_patches)
+            model_outputs_patches = self.model(ray_bundle_patches)
             rgb_patches = (
                 model_outputs_patches["rgb"]
                 .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 3)
@@ -140,22 +160,23 @@ class SemanticNerfactoPipeline(VanillaPipeline):
                 .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 1)
                 .permute(2, 0, 1, 3)[..., 0]
             )  # (num_patches, patch_resolution, patch_resolution)
+            # Hardcode 134 as dimension since this is the number of classes
             semantics_patches = (
                 model_outputs_patches["semantics"]
-                .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 1)
+                .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 134)
                 .permute(2, 0, 1, 3)[..., 0]
             )  # (num_patches, patch_resolution, patch_resolution)
 
         if self.config.use_regnerf_depth_loss:
             regnerf_loss = self.apply_regnerf_loss(step, depth_patches)
-            loss_dict["regnerf_depth_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+            loss_dict["regnerf_depth_loss"] = self.config.regnerf_depth_loss_mult * regnerf_loss
             
         if self.config.use_regnerf_rgb_loss:
             regnerf_loss = self.apply_regnerf_loss(step, rgb_patches)
-            loss_dict["regnerf_rgb_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+            loss_dict["regnerf_rgb_loss"] = self.config.regnerf_rgb_loss_mult * regnerf_loss
             
         if self.config.use_regnerf_semantics_loss:
             regnerf_loss = self.apply_regnerf_loss(step, semantics_patches)
-            loss_dict["regnerf_semantics_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+            loss_dict["regnerf_semantics_loss"] = self.config.regnerf_semantics_loss_mult * regnerf_loss
         
         return model_outputs, loss_dict, metrics_dict
