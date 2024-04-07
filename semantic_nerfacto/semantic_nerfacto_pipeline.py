8c12035d3369491cde61b3582dc765cf939d0368
AlexanderVoldby <alexander@murmann.dk>
2024-04-07 10:55:38 +0200

    Added regnerf loss for depth, rgb and semantics

--- semantic_nerfacto/semantic_nerfacto_pipeline.py
+++ semantic_nerfacto/semantic_nerfacto_pipeline.py
@@ -6,6 +6,7 @@ import torch
 import typing
 from dataclasses import dataclass, field
 from typing import Literal, Optional, Type
+from torchtyping import TensorType
 
 import torch.distributed as dist
 from torch.cuda.amp.grad_scaler import GradScaler
@@ -24,6 +25,7 @@ from nerfstudio.pipelines.base_pipeline import (
     VanillaPipeline,
     VanillaPipelineConfig,
 )
+from nerfstudio.utils import profiler
 
 
 @dataclass
@@ -36,7 +38,17 @@ class SemanticNerfactoPipelineConfig(VanillaPipelineConfig):
     """specifies the datamanager config"""
     model: ModelConfig = SemanticNerfactoModelConfig()
     """specifies the model config"""
-
+    use_regnerf_depth_loss: bool = True
+    """Whether to use reqgularization on depth patches"""
+    use_regnerf_rgb_loss: bool = True
+    """Whether to use regularization on RGB patches"""
+    use_regnerf_semantics_loss: bool = True
+    """Whether to use regularization on RGB patches"""
+    regnerf_depth_loss_mult = 1e-6
+    regnerf_rgb_loss_mult = 1e-6
+    regnerf_semantics_loss_mult = 1e-6
+    """Loss multipliers for the regularization. Do i need to finetune these?"""
+    
 
 class SemanticNerfactoPipeline(VanillaPipeline):
     """Template Pipeline
@@ -81,9 +93,17 @@ class SemanticNerfactoPipeline(VanillaPipeline):
     
     # TODO: Add total variation loss as included in Nerfbusters
     # Requires getting the loss_dict from the model and adding the TV-loss
+    
+    def apply_regnerf_loss(self, step: int, patches_density: TensorType["num_patches", "res", "res"]):
+        pd = patches_density
+        delta_x = pd[..., :-1, 1:] - pd[..., :-1, :-1]
+        delta_y = pd[..., 1:, :-1] - pd[..., :-1, :-1]
+        loss = torch.mean(delta_x**2 + delta_y**2)
+        return loss
+    
     @profiler.time_function
     def get_train_loss_dict(self, step: int):
-        model_outputs, loss_dict, metrics_dict = self.get_standard_loss_dict(step)
+        model_outputs, loss_dict, metrics_dict = super().get_train_loss_dict(step)
 
         # --------------------- 2D losses ---------------------
         activate_patch_sampling = self.config.use_regnerf_loss
@@ -109,7 +129,7 @@ class SemanticNerfactoPipeline(VanillaPipeline):
             )  # (patch_resolution, patch_resolution, num_patches)
             ray_bundle_patches = ray_bundle_patches.flatten()
             # TODO: deal with appearance embeddings
-            model_outputs_patches = self.model(ray_bundle_patches)
+            model_outputs_patches = self.model.get_outputs(ray_bundle_patches)
             rgb_patches = (
                 model_outputs_patches["rgb"]
                 .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 3)
@@ -120,7 +140,22 @@ class SemanticNerfactoPipeline(VanillaPipeline):
                 .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 1)
                 .permute(2, 0, 1, 3)[..., 0]
             )  # (num_patches, patch_resolution, patch_resolution)
+            semantics_patches = (
+                model_outputs_patches["semantics"]
+                .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 1)
+                .permute(2, 0, 1, 3)[..., 0]
+            )  # (num_patches, patch_resolution, patch_resolution)
 
-        if self.config.use_regnerf_loss:
+        if self.config.use_regnerf_depth_loss:
             regnerf_loss = self.apply_regnerf_loss(step, depth_patches)
-            loss_dict["regnerf_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+            loss_dict["regnerf_depth_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+            
+        if self.config.use_regnerf_rgb_loss:
+            regnerf_loss = self.apply_regnerf_loss(step, rgb_patches)
+            loss_dict["regnerf_rgb_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+            
+        if self.config.use_regnerf_semantics_loss:
+            regnerf_loss = self.apply_regnerf_loss(step, semantics_patches)
+            loss_dict["regnerf_semantics_loss"] = self.config.regnerf_loss_mult * regnerf_loss
+        
+        return model_outputs, loss_dict, metrics_dict
