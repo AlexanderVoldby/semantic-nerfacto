c5b6d9d180edaff1b34137935739546ebd6d3d70
AlexanderVoldby <alexander@murmann.dk>
2024-04-20 14:25:45 +0200

    Added tv-loss to semantic-depth-nerfacto-pipeline

--- semantic_nerfacto/semantic_depth_nerfacto_pipeline.py
+++ semantic_nerfacto/semantic_depth_nerfacto_pipeline.py
@@ -5,6 +5,8 @@ Pipeline for depth nerfacto. Very similar to vanilla pipeline
 import typing
 from dataclasses import dataclass, field
 from typing import Literal, Optional, Type
+from torchtyping import TensorType
+import torch
 
 import torch.distributed as dist
 from torch.cuda.amp.grad_scaler import GradScaler
@@ -16,12 +18,18 @@ from nerfstudio.data.datamanagers.base_datamanager import (
     DataManager,
     DataManagerConfig,
 )
+from semantic_nerfacto.utils.random_train_pose import random_train_pose
+
+from nerfstudio.data.datamanagers.base_datamanager import (
+    DataManager,
+    DataManagerConfig,
+)
 from nerfstudio.models.base_model import ModelConfig
 from nerfstudio.pipelines.base_pipeline import (
     VanillaPipeline,
     VanillaPipelineConfig,
 )
-
+from nerfstudio.utils import profiler
 
 @dataclass
 class SemanticDepthNerfactoPipelineConfig(VanillaPipelineConfig):
@@ -75,3 +83,74 @@ class SemanticDepthNerfactoPipeline(VanillaPipeline):
                 SemanticDepthNerfactoModel, DDP(self._model, device_ids=[local_rank], find_unused_parameters=True)
             )
             dist.barrier(device_ids=[local_rank])
+    
+    # TODO: Add total variation loss as included in Nerfbusters
+    # Requires getting the loss_dict from the model and adding the TV-loss
+    
+    def apply_regnerf_loss(self, step: int, patches_density: TensorType["num_patches", "res", "res"]):
+        pd = patches_density
+        delta_x = pd[..., :-1, 1:] - pd[..., :-1, :-1]
+        delta_y = pd[..., 1:, :-1] - pd[..., :-1, :-1]
+        loss = torch.mean(delta_x**2 + delta_y**2)
+        return loss
+    
+    @profiler.time_function
+    def get_train_loss_dict(self, step: int):
+        model_outputs, loss_dict, metrics_dict = super().get_train_loss_dict(step)
+
+        # --------------------- 2D losses ---------------------
+        # Assume we always want patch sampling
+        activate_patch_sampling = self.config.use_regnerf_depth_loss or self.config.use_regnerf_rgb_loss or self.config.use_regnerf_semantics_loss
+
+        # TODO: debug why patch sampling decreases model performance
+        if activate_patch_sampling:
+            cameras, vertical_rotation, central_rotation = random_train_pose(
+                size=self.config.num_patches,
+                resolution=self.config.patch_resolution,
+                device=self.device,
+                radius_mean=self.config.aabb_scalar,  # no sqrt(3) here
+                radius_std=0.0,
+                central_rotation_range=self.config.central_rotation_range,
+                vertical_rotation_range=self.config.vertical_rotation_range,
+                focal_range=self.config.focal_range,
+                jitter_std=self.config.jitter_std,
+                center=self.config.center,
+            )
+            # TODO: fix indices
+            camera_indices = torch.tensor(list(range(self.config.num_patches))).unsqueeze(-1)
+            ray_bundle_patches = cameras.generate_rays(
+                camera_indices
+            )  # (patch_resolution, patch_resolution, num_patches)
+            ray_bundle_patches = ray_bundle_patches.flatten()
+            # TODO: deal with appearance embeddings
+            model_outputs_patches = self.model(ray_bundle_patches)
+            rgb_patches = (
+                model_outputs_patches["rgb"]
+                .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 3)
+                .permute(2, 0, 1, 3)
+            )  # (num_patches, patch_resolution, patch_resolution, 3)
+            depth_patches = (
+                model_outputs_patches["depth"]
+                .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 1)
+                .permute(2, 0, 1, 3)[..., 0]
+            )  # (num_patches, patch_resolution, patch_resolution)
+            # Hardcode 134 as dimension since this is the number of classes
+            semantics_patches = (
+                model_outputs_patches["semantics"]
+                .reshape(self.config.patch_resolution, self.config.patch_resolution, self.config.num_patches, 134)
+                .permute(2, 0, 1, 3)[..., 0]
+            )  # (num_patches, patch_resolution, patch_resolution)
+
+        if self.config.use_regnerf_depth_loss:
+            regnerf_loss = self.apply_regnerf_loss(step, depth_patches)
+            loss_dict["regnerf_depth_loss"] = self.config.regnerf_depth_loss_mult * regnerf_loss
+            
+        if self.config.use_regnerf_rgb_loss:
+            regnerf_loss = self.apply_regnerf_loss(step, rgb_patches)
+            loss_dict["regnerf_rgb_loss"] = self.config.regnerf_rgb_loss_mult * regnerf_loss
+            
+        if self.config.use_regnerf_semantics_loss:
+            regnerf_loss = self.apply_regnerf_loss(step, semantics_patches)
+            loss_dict["regnerf_semantics_loss"] = self.config.regnerf_semantics_loss_mult * regnerf_loss
+        
+        return model_outputs, loss_dict, metrics_dict
